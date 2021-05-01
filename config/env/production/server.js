e263f0f26d2a58bc9ef8f1c543f88e34f8751bf2
leonunesbs <leonunesbs@gmail.com>
2021-05-01 01:55:39 -0300

    provider

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,15 +1,3 @@
-const forgotPasswordTemplate = require('./email-templates/forgot-password');
-
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
   url: env('HEROKU_URL'),
-  admin: {
-    serveAdminPanel: true, // http://yourbackend.com will not serve any static admin files
-    forgotPassword: {
-      from: 'leonunesbs@gmail.com',
-      replyTo: 'leonunesbs@gmail.com',
-      emailTemplate: forgotPasswordTemplate,
-    },
-  },
 });
